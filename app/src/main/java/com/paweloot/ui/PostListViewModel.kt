58696ecea46d84099323bfdf65d17fe59f7311c2
Paweł Banaś <paweloot@gmail.com>
2020-01-26 11:07:58 +0100

    Subscribe to get post list call

--- app/src/main/java/com/paweloot/ui/PostListViewModel.kt
+++ app/src/main/java/com/paweloot/ui/PostListViewModel.kt
@@ -2,6 +2,9 @@ package com.paweloot.ui
 
 import com.paweloot.base.BaseViewModel
 import com.paweloot.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class PostListViewModel : BaseViewModel() {
@@ -9,5 +12,42 @@ class PostListViewModel : BaseViewModel() {
     @Inject
     lateinit var postApi: PostApi
 
+    private lateinit var subscription: Disposable
 
+    init {
+        loadPosts()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
+    private fun loadPosts() {
+        subscription = postApi.getPosts()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { onRetrievePostListSuccess() },
+                { onRetrievePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart() {
+
+    }
+
+    private fun onRetrievePostListFinish() {
+
+    }
+
+    private fun onRetrievePostListSuccess() {
+
+    }
+
+    private fun onRetrievePostListError() {
+
+    }
 }
